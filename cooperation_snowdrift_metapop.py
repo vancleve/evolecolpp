1b5d429dba7e2c115d3d99aff19c7f565b6696ba
vancleve <jeremy.vancleve@gmail.com>
2015-06-16 04:33:31 -0400

    updated plotting stuff

--- cooperation_snowdrift_metapop.py
+++ cooperation_snowdrift_metapop.py
@@ -20,18 +20,42 @@ def evolve(rng, ngens, N, z0, mu, scale, m, b1, b2, c1, c2, grain=1):
     
     return muts
 
-def plotEvolve(vals, bins=100, interpolation='bicubic'):
+def plotEvolve(vals, bins=100, interpolation='bicubic', vmin=None, vmax=None, tmult=1):
     parr = np.array(vals)
-    times = np.repeat(np.arange(parr.shape[0]), parr.shape[1]*parr.shape[2])
-    plist = parr.ravel()
-    
-    phist, xe, ye = np.histogram2d(plist, times, bins=bins, range=[[0,1], [0, len(vals)]])
-    
-    plt.imshow(phist.T, aspect='auto', interpolation=interpolation,
-               origin='lower', cmap=plt.cm.binary, extent=(min(xe),max(xe),min(ye),max(ye)))
+    nruns = parr.shape[0]
+
+    phist = np.zeros((nruns,bins))
+    for i in xrange(nruns):
+        ph, xe = np.histogram(parr[i].ravel(), bins=bins, range=[0,1])
+        phist[i] = ph
+
+    extent=(min(xe),max(xe),0,nruns)
+    plt.imshow(phist, aspect='auto', interpolation=interpolation,
+               origin='lower', cmap=plt.cm.binary, extent=(min(xe),max(xe),0,nruns),
+               vmin=vmin, vmax=vmax)
+    yticks(around(linspace(0,nruns,11),-1), around(linspace(0,(nruns-1)*tmult+1,11),-1))
     plt.show()
 
+# Plot with surface
+# def plotEvolve(vals, bins=100, interpolation='bicubic', elev=30, azim=-60, alpha=0.7):
+#     parr = np.array(vals)
+#     nruns = parr.shape[0]
 
+#     phist = np.zeros((nruns,bins))
+#     for i in xrange(nruns):
+#         ph, xe = np.histogram(parr[i].ravel(), bins=bins, range=[0,1])
+#         phist[i] = ph
+
+#     x = (xe[0:-1] + xe[1:]) / 2
+#     x, y = np.meshgrid(x, range(nruns))
+
+#     fig = plt.figure()
+#     ax = fig.gca(projection='3d')
+#     ax.plot_surface(x, y, phist, alpha=alpha, rstride=10, cstride=1, edgecolors='none')
+#     #ax.plot_wireframe(x, y, phist, rstride=100, cstride=1)
+#     ax.view_init(elev=elev, azim=azim)
+#     plt.show()
+    
 # fix the seed and initialize the random number generator
 SEED=314
 gsl_rng = snow.GSLrng(SEED)
@@ -43,6 +67,6 @@ except: # run a sim and plot (if not loaded into ipython)
     # Generate Figure 1A from Doebeli, Hauert, and Killingback (2004, Science)
     # (noisier than Doebeli et al. due to smaller population size and higher mutation)
     w=10 # 'strength' of selection
-    muts = evolve(gsl_rng, 100, [10]*500, 0.01, 0.01, 0.02, 0.6, 6.0*w, -1.4*w, 4.56*w, -1.6*w, grain=1)
+    muts = evolve(gsl_rng, 8000, [20]*250, 0.01, 0.05, 0.01, 0.6, 6.0*w, -1.4*w, 4.56*w, -1.6*w, grain=1)
     
-    # plotEvolve(muts)
+    plotEvolve(muts)
